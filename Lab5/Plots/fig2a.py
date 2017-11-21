fb952a17a241ca4554e11259c1a4fbcb7102fe58
Zach Colbert <colbertz@oregonstate.edu>
2017-11-21 13:04:24 -0800

    Did some weird polyfit math

--- Lab5/Plots/fig2a.py
+++ Lab5/Plots/fig2a.py
@@ -9,16 +9,21 @@ dataPath = os.path.dirname(os.path.abspath(__file__)) + '/../Data'
 time, mathFxn = np.genfromtxt((dataPath + '/2a.csv'), unpack=True, delimiter=',', skiprows=1, usecols=(0,1))
 
 # Do some math
-f = 100 #Hz
-A = .6 / 2 # 600 mVpp / 2 makes wave amplitude
-inputSignal = -1 * A * np.sin(2 * np.pi * f * time)
+# f = 100 #Hz
+# A = .6 / 2 # 600 mVpp / 2 makes wave amplitude
+# inputSignal = -1 * A * np.sin(2 * np.pi * f * time)
 
-outputSignal = inputSignal - mathFxn
+# outputSignal = inputSignal - mathFxn
+
+# Smoothed line for plot?
+smoothFxn = np.poly1d(np.polyfit(time, mathFxn, 30))
 
 # Plot the stuff
 plt.figure()
 plt.plot(time, mathFxn, 'r,')
-plt.plot(time, inputSignal)
-plt.plot(time, outputSignal, 'g')
+plt.plot(time, smoothFxn(time))
+# plt.plot(time, inputSignal)
+# plt.plot(time, outputSignal, 'g')
+plt.ylim(-2.5, 2.5)
 
 plt.show()
